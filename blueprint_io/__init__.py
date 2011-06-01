981d94776b4f56224edfd2a88a25ba5e4f26e999
Richard Crowley <r@rcrowley.org>
2011-06-01 22:04:45 +0000

    Testing out potential CLI argument changes.

--- blueprint_io/__init__.py
+++ blueprint_io/__init__.py
@@ -1,21 +1,23 @@
 import cfg
 import json
 import logging
+import sys
 import urlparse
-
-import http
 
 from blueprint import git
 from blueprint import Blueprint
 from blueprint import context_managers
+import http
+
 
 server = cfg.server()
 
-def pull(url):
+
+def pull(secret, name):
     """
-    Pull a blueprint from the specified URL
+    Pull a blueprint from the secret and name on the configured server.
     """
-    
+
     # GET /secret/name
     # Fetch the JSON representation of the blueprint name.
     # 
@@ -30,107 +32,69 @@ def pull(url):
     # secret: a 64-byte identifier containing numbers, letters, underscores, and dashes.
     # name: a blueprint name; it may not contain whitespace or / characters.
     # sha: a 40-byte hexadecimal representation of a SHA1 sum.
+    r = http.get('/{0}/{1}'.format(secret, name))
+    if 200 == r.status:
+        b = Blueprint()
+        b.name = name
+        b.update(json.loads(r.read()))
 
-    pieces = urlparse.urlparse(url)
-    server = pieces.netloc
-    url = pieces.path
+        for filename in b.sources.itervalues():
+            r = http.get('/{0}/{1}/{2}'.format(secret, name, filename))
+            if 200 == r.status:
+                try:
+                    f = open(filename, 'w')
+                    f.write(r.read())
+                except OSError:
+                    logging.error('could not open {0}'.format(filename))
+                    return None
+                finally:
+                    f.close()
+            elif 404 == r.status:
+                logging.error('{0} not found'.format(filename))
+                return None
+            elif 502 == r.status:
+                logging.error('upstream storage service failed')
+                return None
+            else:
+                logging.error('unexpected {0} fetching tarball'
+                              ''.format(r.status))
+                return None
 
-    with context_managers.mkdtemp():
-        r = http.get(url, {}, server)
-        logging.info("Connected to blueprint-io server at %s" % server)
-        if r.status == 200:
-            b = Blueprint()
-            b.name = name = pieces.path.rpartition('/')[2]
-            b.update(json.loads(r.read()))
-            logging.info("Blueprint json pulled, checking for tarballs")
-
-            for filename in b.sources.itervalues():
-                logging.info("Downloading %s, this might take a while" % filename)
-                r = http.get(url + '/' + filename, {}, server)
-
-                if r.status == 200:
-                    try:
-                        tarfile = open(filename, 'w')
-                        tarfile.write(r.read())
-                        logging.info("Blueprint tarballs pulled")
-                    except OSError:
-                        return
-                    finally:
-                        tarfile.close()
-
-                elif r.status == 404:
-                    logging.error("[404] The sha tarball was not found")
-                    return
-
-                elif r.status == 502:
-                    logging.error("[502] The upstream storage service failed and the blueprint was not pulled")
-                    return
-
-                else:
-                    logging.error("[%s] GET error retreiving blueprint files" % r.status)
-                    return
-            
-            logging.info('Storing blueprint on this machine')
-            b.commit('')
-            logging.info('Success! Blueprint pulled and is ready for use.')
-
-        elif r.status == 404:
-            logging.error("[404] A blueprint could not be pulled from %s" % pieces.netloc + pieces.path)
-            return
-
-        else:
-            logging.error("[%s] GET error" % r.status)
-            return
-
-    return
-
-
-def push(b):
-    """
-    Push a blueprint to server specified in /etc/blueprint-io.cfg,
-    ~/.blueprint-io.cfg or default to https://devstructure.com
-    """
-
-    # GET /secret
-    # Create a new secret key known only to the caller. 
-	# This is the namespace beneath which the caller’s blueprints are stored.
-    
-    r = http.get("/secret")
-    logging.info("Connected to blueprint-io server at %s" % server)
-    if r.status == 201:
-        secret = r.read().rstrip()
-        logging.info("Secret key: %s" % secret)
-    elif r.status == 502:
-        logging.error('[502] GET failure; the upstream storage service failed')
-        return
+        return b
+    elif 404 == r.status:
+        logging.error('blueprint not found')
+    elif 502 == r.status:
+        logging.error('upstream storage service failed')
     else:
-        logging.error('[%s] GET failure' % r.status)
-        return
-   
+        logging.error('unexpected {0} fetching blueprint'.format(r.status))
+    return None
+
+
+def push(secret, b):
+    """
+    Push a blueprint to the secret and its name on the configured server.
+    """
+
     # PUT /secret/name
     #  Store the JSON representation of the blueprint name. The Content-Type of the body must be application/json.
     # 
     #  Parameters:
     #  secret: a 64-byte identifier containing numbers, letters, underscores, and dashes.
     #  name: a blueprint name; it may not contain whitespace or / characters.
-    
-    r = http.put(
-        url = '/' + secret + '/' + b.name,
-        body = b.dumps(),
-        headers = {'Content-type': 'application/json'} )
-
-    if r.status == 202:
-        logging.info('Blueprint JSON stored on server, moving on to the blueprint tarballs')
-    elif r.status == 400:
-        logging.error('[400] PUT failure; the blueprint was not well-formed')
-        return
-    elif r.status == 502:
-        logging.error('[502] JSON PUT failure; the upstream storage service failed')
-        return
+    r = http.put('/{0}/{1}'.format(secret, b.name),
+                 b.dumps(),
+                 {'Content-Type': 'application/json'})
+    if 202 == r.status:
+        pass
+    elif 400 == r.status:
+        logging.error('malformed blueprint')
+        return None
+    elif 502 ==  r.status:
+        logging.error('upstream storage service failed')
+        return None
     else:
-        logging.error('[%s] JSON PUT failure' % r.status)
-        return
-    
+        logging.error('unexpected {0} storing blueprint'.format(r.status))
+        return None
 
     # PUT /secret/name/sha.tar
     # Store a source tarball referenced by blueprint name. The Content-Type of the body must be application/x-tar.
@@ -139,27 +103,45 @@ def push(b):
     # secret: a 64-byte identifier containing numbers, letters, underscores, and dashes.
     # name: a blueprint name; it may not contain whitespace or / characters.
     # sha: a 40-byte hexadecimal representation of a SHA1 sum.
-
     tree = git.tree(b._commit)
     for dirname, filename in sorted(b.sources.iteritems()):
         blob = git.blob(tree, filename)
         content = git.content(blob)
-        logging.info("Uploading %s, this might take a while" % filename)
-        r = http.put(
-            url = '/' + secret + '/' + b.name + '/' + filename,
-            body = content,
-            headers = {'Content-type': 'application/x-tar'})
+        r = http.put('/{0}/{1}/{2}'.format(secret, b.name, filename),
+                     content,
+                     {'Content-Type': 'application/x-tar'})
+        if 202 == r.status:
+            pass
+        elif 400 == r.status:
+            logging.error('tarball content or name not expected')
+            return None
+        elif 404 == r.status:
+            logging.error('blueprint not found')
+            return None
+        elif 502 == r.status:
+            logging.error('upstream storage service failed')
+            return None
+        else:
+            logging.error('unexpected {0} storing tarball'.format(r.status))
+            return None
 
-    if r.status == 202:
-        logging.info('Success! Blueprint pushed and available at: %s' % server + '/' + secret + '/' + b.name)
-    elif r.status == 400:
-        logging.error('[400] tarball PUT failure; the SHA1 sum of the body did not match sha')
-    elif r.status == 404:
-        logging.error('[404] tarball PUT failure; the secret or blueprint name was not found')
-    elif r.status == 502:
-        logging.error('[502] tarball PUT failurel the upstream storage service failed')
+    return '{0}/{1}/{2}'.format(server, secret, b.name)
+
+
+def secret():
+    """
+    Fetch a new secret from the configured server.
+    """
+    r = http.get('/secret')
+    if 201 == r.status:
+        secret = r.read().rstrip()
+        logging.warning('created secret {0}'.format(secret))
+        logging.warning('store it in ~/.blueprint-io.cfg:')
+        sys.stderr.write('[default]\nsecret = {0}\n\n'.format(secret))
+        return secret
+    elif 502 == r.status:
+        logging.error('upstream storage service failed')
+        return None
     else:
-        logging.error('[%s] tarball PUT failure' % r.status)
-    
-    return
-    
+        logging.error('unexpected {0} creating secret'.format(r.status))
+        return None
--- blueprint_io/cfg.py
+++ blueprint_io/cfg.py
@@ -1,21 +1,27 @@
-from ConfigParser import ConfigParser
+from ConfigParser import ConfigParser, NoOptionError
 import os.path
 import sys
 
+
 # Parse the configuration file for a valid Blueprint I/O Server.
-# If a server is not specified locally, use https://devstructure.com
-# Local configuration files /etc/blueprint-io.cfg or ~/.blueprint-io.cfg
-
-cfg = ConfigParser(defaults={'server': 'http://127.0.0.1:5000'})
-
+cfg = ConfigParser(defaults={'server': 'https://devstructure.com'})
 getattr(cfg, '_sections')['default'] = getattr(cfg, '_dict')()
 cfg.read(['/etc/blueprint-io.cfg',
           os.path.expanduser('~/.blueprint-io.cfg')])
-
 for option in ('server',):
     if not cfg.has_option('default', option):
         sys.stderr.write('cfg: missing {0}\n'.format(option))
         sys.exit(1)
+
+
+def secret():
+    """
+    Return the configured secret.
+    """
+    try:
+        return cfg.get('default', 'secret')
+    except NoOptionError:
+        return None
 
 def server():
     """
